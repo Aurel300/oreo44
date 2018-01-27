8fe7595145d59bae996ef528e5a59cdc8506fdb7
Aurel Bílý <aurel.bily@gmail.com>
2018-01-27 14:39:05 +0000

    initial

---
+++ src/input/InputMethod.hx
@@ -0,0 +1,5 @@
+package input;
+
+interface InputMethod {
+  function tick():InputState;
+}
---
+++ src/input/InputState.hx
@@ -0,0 +1,7 @@
+package input;
+
+enum InputState {
+  Joystick(x:Float, y:Float);
+  Wheel(a:Float);
+  Slider(i:Float);
+}
